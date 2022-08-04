8d7826460b3b7ce3e83646a0888beb5398b2ec7d
Lucas Rachadel Andre <lucas.andre@projuris.com.br>
2022-08-04 13:41:44 -0300

    Adicionado Controller e excluido DTO

--- src/main/java/com/lracha/libraryapi/controllers/LivrosController.java
+++ src/main/java/com/lracha/libraryapi/controllers/LivrosController.java
@@ -1,16 +1,34 @@
 package com.lracha.libraryapi.controllers;
 
-import com.lracha.libraryapi.DTOs.LivroDTO;
+import com.lracha.libraryapi.Repository.LivrosRepository;
+import com.lracha.libraryapi.model.Livros;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/library")
 public class LivrosController {
-    @PostMapping
-    public ResponseEntity<Object> novoLivros(@RequestBody @Valid LivroDTO livroDTO){
 
+    @Autowired
+    private LivrosRepository livrosRepository;
+    @PostMapping
+    public Livros postLivro(@RequestBody @Valid Livros livros){
+        return livrosRepository.save(livros);
+    }
+
+    @GetMapping("/livro/{id}")
+    public ResponseEntity<Livros> getById(UUID id){
+        if(livrosRepository.existsById(id)){
+            return new ResponseEntity<Livros>(livrosRepository.getById(id), HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
